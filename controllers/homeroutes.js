cffd542288d05c40aa20323c9fa9dc0432572d57
guitarkeegan <keegananglim@gmail.com>
2022-07-19 08:00:15 -0700

    add comments from homeroutes

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -13,24 +13,24 @@ router.get("/", async (req, res) => {
     res.json(err);
   }
 });
-
+// dashboard is where users can create, edit and delete posts that they have written. Only logged in users can access this route.
 router.get("/dashboard", withAuth, async (req, res) => {
-
+// find all posts where the current user matches the author of a post in the database.
     const personalPosts = await Blog.findAll({
       where: {
         user_id: req.session.user_id,
       },
     });
-
+//Get only the simplified objects from the database.
     const allBlogs = personalPosts.map((post) => post.get({ plain: true }));
     res.render("dashboard", { allBlogs, loggedIn: req.session.loggedIn });
   } 
 );
-
+// Only authenticated users can access the create-post page. On this page, users can upload a post image, and fill in the title, subtitle, and content of their blog post.
 router.get("/create-post", withAuth, async (req, res)=>{
   res.render('createpost', {loggedIn: req.session.loggedIn});
 });
-
+//Here we allow authenticated users to edit a post that they have written.
 router.get("/edit-post/:id", withAuth, async (req, res)=>{
 
   try {
@@ -41,7 +41,7 @@ router.get("/edit-post/:id", withAuth, async (req, res)=>{
     res.status(400).json(err);
   }
 })
-
+// Here, users can access a single blog post by id. If they are logged in, they can also leave a comment.
 router.get("/blog/:id", async (req, res) => {
   try {
     const blogData = await Blog.findByPk(req.params.id, {
@@ -66,7 +66,7 @@ router.get("/blog/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+// users with accounts can login here, or else they are redirected back to the homepage.
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
@@ -74,7 +74,7 @@ router.get("/login", (req, res) => {
   }
   res.render("login");
 });
-
+// Users whose emails are not in the database, can sign up with a username, email, and password.
 router.get("/signup", (req, res) => {
   if (!req.session.loggedIn) {
     res.render("signup");
